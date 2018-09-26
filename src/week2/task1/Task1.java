6eb810520c731c38e6443689b2e8ef6efd369b70
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-09-26 15:43:41 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,8 +1,10 @@
-﻿package week2.task1;
+package week2.task1;
+import java.util.Scanner;
 
-
+/**
+ * Created by CCNE on 26/09/2018.
+ */
 public class Task1 {
-
     public static int gcd(int a, int b) {
         while(a!=b)
         {
@@ -14,7 +16,7 @@ public class Task1 {
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        int a=0,b=1,c;
+        int a=0,b=1,c = 0;
         for(int i=1;i<n;i++)
         {
             c= a + b;
@@ -22,5 +24,17 @@ public class Task1 {
             b= c;
         }
         return c;
+    }
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap A: ");
+        a= sc.nextInt();
+        System.out.println("nhap B:");
+        b= sc.nextInt();
+
+
+        System.out.println("uoc trung lon nhat la: "+ gcd(a,b) );
+        System.out.println(fibonacci(a));
     }
 }
