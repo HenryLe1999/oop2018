cdaf66d5347df9b8b93df3111924292bdcf3b892
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 16:34:32 +0700

    Create Cam.java

---
+++ src/week4/task1/Cam.java
@@ -0,0 +1,11 @@
+package week4.task1;
+
+public class Cam extends HoaQua{
+    Cam() {
+        hinhdang = "tron";
+        khoiluong = 50;
+        giaban = 5000;
+        mau = "vang";
+        nguongoc = "Viet Nam";
+    }
+}
