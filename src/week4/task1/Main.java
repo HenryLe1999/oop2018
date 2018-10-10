26122370e7329e2cefb3083a94342d95f2a08529
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 18:12:12 +0700

    Update Main.java

--- src/week4/task1/Main.java
+++ src/week4/task1/Main.java
@@ -4,23 +4,23 @@ public class Main {
     public static void main(String[] args) {
 
         HoaQua h = new HoaQua();
-        HoaQua h2 = new HoaQua("qua Cam", "Hai Duong");
-        HoaQua h1 = new HoaQua("qua Tao","Hai Duong");
+        HoaQua h2 = new HoaQua("qua Cam", "NInh BInh");
+        HoaQua h1 = new HoaQua("qua Tao","NInh Binh");
         h.gioiThieu();//in ra thong tin cua h tu ham gioiThieu
         System.out.println("\n");
         QuaCam c = new QuaCam();
-        QuaCam c1 = new QuaCam("Ha Noi",10);
-        QuaCam c2 = new QuaCam("Hai Duong",20);
+        QuaCam c1 = new QuaCam("Nghe an",10);
+        QuaCam c2 = new QuaCam("Da Nang",20);
         c.gioiThieu();//in ra thong tin cua c tu ham gioiThieu
         c.kiemTra(c1,c2);//kiem tra xem c1 c2 co cung noi khong
         System.out.println("\n");
         CamCaoPhong cp = new CamCaoPhong();
         CamCaoPhong cam1 = new CamCaoPhong();
-        cam1.setTenqua("Cam Cao Phong hd");
-        cam1.setNoinhap("Hai Duong");
+        cam1.setTenqua("Cam Cao Phong NinhBinh");
+        cam1.setNoinhap("NInh BInh");
         CamCaoPhong cam2 = new CamCaoPhong();
-        cam2.setTenqua("Cam Cao Phong hy");
-        cam2.setNoinhap("Hung Yen");
+        cam2.setTenqua("Cam Cao Phong Ha Noi");
+        cam2.setNoinhap("Ha NOi");
         CamCaoPhong cam = new CamCaoPhong(20,"ngot");
         cp.gioiThieucamcaophong(cam);
         System.out.println("\n");
