7c0d58f8946af8db7cdd95ba30119a6dd61736cf
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 16:58:52 +0700

    Update Cam.java

--- src/week4/task1/Cam.java
+++ src/week4/task1/Cam.java
@@ -2,10 +2,8 @@ package week4.task1;
 
 public class Cam extends HoaQua{
     Cam() {
-        hinhdang = "tron";
         khoiluong = 50;
         giaban = 5000;
         mau = "vang";
-        nguongoc = "Viet Nam";
     }
 }
