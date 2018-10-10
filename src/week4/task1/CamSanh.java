788faa46f9491f7e689fbd11fe3a04c7fdf60053
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 17:48:24 +0700

    Update CamSanh.java

--- src/week4/task1/CamSanh.java
+++ src/week4/task1/CamSanh.java
@@ -1,9 +1,27 @@
 package week4.task1;
 
-public class CamSanh extends HoaQua{
-    CamSanh() {
-        khoiluong = 100;
-        giaban = 23000;
-        mau = "xanh";
+public class CamSanh extends QuaCam{
+    private double can;
+    private String soluong;
+//khai bao phuong thuc can
+    public double getCan() {
+        return can;
+    }
+
+    public void setCan(double can) {
+        this.can = can;
+    }
+ //  khai bao phuong thuc so luong
+    public String getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(String soluong) {
+        this.soluong = soluong;
+    }
+
+    public void gioiThieucamsanh(CamCaoPhong c1){
+        //in ra thong tin cua hoa qua bao gom ngay nhap va so luong
+        System.out.println("Ngay nhap" + c1.getNoinhap() + ", So luong nhap: " + getSoluong());
     }
 }
