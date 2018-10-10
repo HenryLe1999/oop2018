39e1ecfed5d73eca34413747253682ad1956f693
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 17:45:54 +0700

    Rename CamSanh.java to Cam.java

--- src/week4/task1/Cam.java
+++ src/week4/task1/Cam.java
@@ -1,9 +1,27 @@
 package week4.task1;
 
-public class Cam extends HoaQua{
-    Cam() { 
-        khoiluong = 50;
-        giaban = 5000;
-        mau = "vang";
+public class CamSanh extends QuaCam{
+    private double can;
+    private String soluong;
+//khai bao phuong thuc can
+    public double getCan() {
+        return can;
+    }
+
+    public void setCan(double can) {
+        this.can = can;
+    }
+ //  khai bao phuong thuc so luong
+    public String getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(String soluong) {
+        this.soluong = soluong;
+    }
+
+    public void gioiThieucamsanh(CamCaoPhong c1){
+        //in ra thong tin cua hoa qua bao gom ngay nhap va so luong
+        System.out.println("Ngay nhap" + c1.getNoinhap() + ", So luong nhap: " + getSoluong());
     }
 }
