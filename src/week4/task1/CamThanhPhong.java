dbc715c4c45018a7e153e17fedeec27107153ea7
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 16:39:15 +0700

    Create CamThanhPhong.java

---
+++ src/week4/task1/CamThanhPhong.java
@@ -0,0 +1,10 @@
+package week4.task1;
+
+public class CamThanhPhong extends HoaQua{
+    CamThanhPhong() {
+        khoiluong = 90;
+        giaban = 40000;
+        mau = "vang";
+        nguongoc = "Thanh Phong";
+    }
+}
