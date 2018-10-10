8ae4a70165b24546ccd2365a04d0ab79402e13f7
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 16:42:35 +0700

    Create CamSanh.java

---
+++ src/week4/task1/CamSanh.java
@@ -0,0 +1,10 @@
+package week4.task1;
+
+public class CamSanh extends HoaQua{
+    CamSanh() {
+        khoiluong = 100;
+        giaban = 23000;
+        mau = "xanh";
+        nguongoc = "Viet Nam";
+    }
+}
