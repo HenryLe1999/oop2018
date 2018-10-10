e3586ac9330e0fa63133fddee9ac04dab1ed64ab
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-10 17:49:32 +0700

    Update QuaTao.java

--- src/week4/task1/QuaTao.java
+++ src/week4/task1/QuaTao.java
@@ -1,14 +1,12 @@
 package week4.task1;
 
-/**
- * Created by CCNE on 10/10/2018.
- */
 public class QuaTao extends HoaQua {
+    //khai bao thuoc tinh cua QuaTao
     private int soluong;
-    private String nguongoc;
+    private String mausac;
 
     public QuaTao(){
-        this.nguongoc = "";
+        this.mausac = "";
         this.soluong = 0 ;
     }
 
@@ -20,20 +18,23 @@ public class QuaTao extends HoaQua {
         this.soluong = soluong;
     }
 
-    public String getNguongoc() {
-        return nguongoc;
+    public String getMausac() {
+        return mausac;
     }
 
-    public void setNguongoc(String nguongoc) {
-        this.nguongoc = nguongoc;
+    public void setMausac(String mausac) {
+        this.mausac = mausac;
     }
 
     public void gioiThieutao(){//Ham gioi thieu thong tin ve tao
         System.out.println("Tao co rat nhieu loai:");
     }
 
-    public void kiemNguoongoc(HoaQua t1, HoaQua t2){
-        if (t1.getNguongoc().equals(t2.getNguongoc()))
+    public void kiemTraten(HoaQua t1, HoaQua t2){
+        //kiem tra xem 2 qua co cung 1 loai hay khong
+        //neu cung loai thi in Cung loai
+        //neu khong cung loai thi in Khac loai
+        if (t1.getTenqua().equals(t2.getTenqua()))
             System.out.println("Cung loai");
         else System.out.println("Khac loai");
 
