2504d77f7c85f82757dd208cbb4ab144827e5a92
HenryLe1999 <43204302+HenryLe1999@users.noreply.github.com>
2018-10-17 17:38:53 +0700

    Update Main.java

--- src/week4/task1/Main.java
+++ src/week4/task1/Main.java
@@ -4,27 +4,27 @@ public class Main {
     public static void main(String[] args) {
 
         HoaQua h = new HoaQua();
-        HoaQua h2 = new HoaQua("qua Cam", "NInh BInh");
-        HoaQua h1 = new HoaQua("qua Tao","NInh Binh");
-        h.gioiThieu();//in ra thong tin cua h tu ham gioiThieu
+        HoaQua h2 = new HoaQua(800, "Xanh");
+        HoaQua h1 = new HoaQua(700,"Vang");
+        h.Thuonghieu();//in ra thong tin cua h tu ham gioiThieu
         System.out.println("\n");
         QuaCam c = new QuaCam();
         QuaCam c1 = new QuaCam("Nghe an",10);
         QuaCam c2 = new QuaCam("Da Nang",20);
-        c.gioiThieu();//in ra thong tin cua c tu ham gioiThieu
+        c.Thuonghieu();//in ra thong tin cua c tu ham gioiThieu
         c.kiemTra(c1,c2);//kiem tra xem c1 c2 co cung noi khong
         System.out.println("\n");
-        CamCaoPhong cp = new CamCaoPhong();
-        CamCaoPhong cam1 = new CamCaoPhong();
-        cam1.setTenqua("Cam Cao Phong NinhBinh");
-        cam1.setNoinhap("NInh BInh");
-        CamCaoPhong cam2 = new CamCaoPhong();
-        cam2.setTenqua("Cam Cao Phong Ha Noi");
-        cam2.setNoinhap("Ha NOi");
-        CamCaoPhong cam = new CamCaoPhong(20,"ngot");
+        CamThanhPhong cp = new CamThanhPhong();
+        CamThanhPhong cam1 = new CamThanhPhong();
+        cam1.setKhoiluong(600);
+        cam1.setHuongvi("Ngot");
+        CamThanhPhong cam2 = new CamThanhPhong();
+        cam2.setKhoiluong(500);
+        cam2.setHuongvi("Chua");
+        CamThanhPhong cam = new CamThanhPhong(600,"ngot");
         cp.gioiThieucamcaophong(cam);
         System.out.println("\n");
-        cp.inCamcaophong(cam1,cam2);
+        cp.inCamThanhphong(cam1,cam2);
 
         System.out.println("\n");
         QuaTao t = new QuaTao();
