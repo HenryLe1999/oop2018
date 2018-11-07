bd53a9fd9954f4c8db3ebdb7d1495bba30158f50
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-11-07 14:27:56 +0700

    baitap

--- src/week9/main.java
+++ src/week9/main.java
@@ -5,7 +5,6 @@ public class main {
         String in = "C:\\Users\\levan.WIN10_SL\\IdeaProjects\\oop20182112018\\nhan.txt";
         String in1 = "C:\\Users\\levan.WIN10_SL\\IdeaProjects\\oop20182112018\\nhan1.txt";
         String in2 = "C:\\Users\\levan.WIN10_SL\\IdeaProjects\\oop20182112018\\nhan2.txt";
-        String in3 = "C:\\Users\\levan.WIN10_SL\\IdeaProjects\\oop20182112018\\nhan3.txt";
         System.out.println("cau a : ");
         Utils.readContentFromFile(in);
         System.out.println("cau b : ");
@@ -13,7 +12,7 @@ public class main {
         System.out.println("cau c");
         Utils.Themvaocuoi(in2);
         System.out.println("cau d");
-        Utils.findFileByName("C:\\Users\\levan.WIN10_SL\\IdeaProjects\\oop20182112018", "nhan2.txt");
+        Utils.findFileByName("C:\\Users\\levan.WIN10_SL\\IdeaProjects\\oop20182112018", "nhan3.txt");
 
     }
 }
